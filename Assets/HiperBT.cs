b266febb1ffd58a93a0b8e8ae3db53c81fdb3b39
elmir <elmir@brab>
2018-11-29 12:32:15 +0100

    use bluetooth C# wrapper code
    disable rtcm3 input mode on the Ntrip A port, otherwise we won't be
    able to enable rtcm3 input mode on the bluetooth port (aka cur/term)

---
+++ Assets/HiperBT.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using brab.bluetooth;
+
+public class HiperBT
+{
+    StreamReader reader;
+    HiperWriter writer;
+
+    class HiperWriter : BinaryWriter
+    {
+        public HiperWriter(Stream stream) : base(stream) {}
+
+        public void WriteStr(string str)
+        {
+            var bytes = Encoding.ASCII.GetBytes(str);
+            Write(bytes, 0, bytes.Length);
+        }
+    }
+
+    public HiperBT()
+    {
+        var ba = BluetoothAdapter.getDefaultAdapter();
+        if (!ba.isEnabled())
+        {
+            throw new Exception("Bluetooth is disabled");
+        }
+
+        BluetoothDevice hiper = null;
+        foreach (var c in ba.getBondedDevices())
+        {
+
+            if (c.getAddress() == "00:07:80:36:02:C6")
+            {
+                hiper = c;
+            }
+        }
+
+        if (hiper == null)
+        {
+            throw new Exception("No hiper device found");
+        }
+
+        var uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+        var sock = hiper.createRfcommSocketToServiceRecord(uuid);
+
+        sock.connect();
+
+        var istream = sock.getInputStream();
+        var ostream = sock.getOutputStream();
+
+        reader = new StreamReader(istream);
+        writer = new HiperWriter(ostream);
+
+        writer.WriteStr("set,/par/dev/ntrip/a/imode,cmd\n\r");
+        writer.WriteStr("print,/par/dev/ntrip/a/imode\n\r");
+        writer.WriteStr("list,/dev\n\r");
+        writer.WriteStr("em,/cur/term,/msg/nmea/GGA:.05\n\r");
+        writer.WriteStr("set,/par/cur/term/imode,rtcm3\n\r");
+        writer.Flush();
+    }
+
+    public string GetNMEA()
+    {
+        return reader.ReadLine();
+    }
+
+    public void PushRTCM(byte[] data, int len)
+    {
+        writer.Write(data, 0, len);
+    }
+}
--- Assets/NMEA.cs
+++ Assets/NMEA.cs
@@ -6,15 +6,13 @@ using UnityEngine.UI;
 public class NMEA : MonoBehaviour
 {
 
-	AndroidJavaClass hiper;
+	HiperBT hiper;
     NTRipClient ntrip;
 
 	void Start ()
     {
 #if !UNITY_EDITOR
-        hiper = new AndroidJavaClass("ws.brab.pl.HiperReader");
-        Debug.LogFormat("Xhyper '{0}'", hiper);
-        hiper.CallStatic("Init");
+        hiper = new HiperBT();
 #endif
 
         ntrip = new NTRipClient("example.com", 8500, "RTCM3_GNSS", "username", "password", PushNtrip);
@@ -23,39 +21,31 @@ public class NMEA : MonoBehaviour
 	void Update ()
     {
 #if !UNITY_EDITOR
-        var line = hiper.CallStatic<string>("GetNMEA");
+        var line = hiper.GetNMEA();
         gameObject.GetComponent<Text>().text = line;
         Debug.LogFormat("hiperLine: '{0}'", line);
-        ntrip.UpdateRoverPosition(line);
+        if (line.StartsWith("$GPGGA"))
+        {
+            Debug.LogFormat("pushing line");
+            ntrip.UpdateRoverPosition(line);
+        }
+        else
+        {
+            Debug.LogFormat("not an NMEA line");
+        }
 //        ntrip.UpdateRoverPosition(55, 14, 60);
 #endif
 	}
 
-    void foo()
-    {
-        byte[] data = new byte[]{
-              4, 10, 11,   2,
-              0, 0,   0, 100,
-            200, 1}
-            ;
-        PushNtrip(data, 8);
-    }
 
     void PushNtrip(byte[] data, int len)
     {
         Debug.LogFormat("pushing {0} bytes", len);
-        string str = "pushedR  : ";
-
-        for (int i = 0; i < len; i += 1)
-        {
-            str += string.Format("{0:x} ", data[i]);
-        }
 
 #if !UNITY_EDITOR
         AndroidJNI.AttachCurrentThread(); // something something, threads, android, dalwik, GC
 
-        hiper.CallStatic("PushRTCM", data, len);
-        Debug.Log(str);
+        hiper.PushRTCM(data, len);
 
         AndroidJNI.DetachCurrentThread();
 #endif
